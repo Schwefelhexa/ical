11fa94f39c1212759605ef9fcbf79ac98ae8c812
Alexander Baron <alexander.baron1@rwth-aachen.de>
2023-11-09 12:25:36 +0100

    Calendar creation UI

--- app/routes/calendars._index.tsx
+++ app/routes/calendars._index.tsx
@@ -1,7 +1,33 @@
 import { ChevronRightIcon } from "@heroicons/react/20/solid";
-import { json } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
+import type { ActionFunctionArgs } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
+import { Form, Link, useLoaderData } from "@remix-run/react";
+import { useState } from "react";
+import Button from "~/components/Button";
+import Dialog from "~/components/Dialog";
+import Input from "~/components/Input";
 import db from "~/db";
+import { calendars } from "~/db/schema";
+
+export async function action({ request }: ActionFunctionArgs) {
+  const formData = await request.formData();
+  const name = formData.get("name")?.toString();
+  const sourceUrl = formData.get("sourceUrl")?.toString();
+
+  if (!name || !sourceUrl || name.length === 0 || sourceUrl.length === 0)
+    return json({ message: "Missing required fields" }, { status: 400 });
+
+  const calendar = await db
+    .insert(calendars)
+    .values({
+      sourceUrl,
+      name,
+    })
+    .returning();
+  const calendarId = calendar[0].id;
+
+  return redirect(`/calendars/${calendarId}`);
+}
 
 export async function loader() {
   const calendars = (await db.query.calendars.findMany()).map((c) => ({
@@ -17,17 +43,18 @@ export async function loader() {
 
 export default function Calendars() {
   const { calendars } = useLoaderData<typeof loader>();
+  const [showDialog, setShowDialog] = useState(false);
 
   return (
     <div className="flex h-full items-center justify-center bg-gray-100">
       <div className="max-w-4xl overflow-hidden bg-white shadow sm:rounded-lg">
-        <div className="px-4 py-5 sm:p-6">
+        <div className="space-y-4 px-4 py-5 sm:p-6">
           <ul className="divide-y divide-gray-100">
             {calendars.map((calendar) => (
               <li key={calendar.id}>
                 <Link
                   to={`/calendars/${calendar.id}`}
-                  className="flex items-center gap-x-4 py-5 px-2 rounded hover:bg-gray-100"
+                  className="flex items-center gap-x-4 rounded px-2 py-5 hover:bg-gray-100"
                 >
                   <img
                     className="h-12 w-12 flex-none rounded-full bg-gray-50"
@@ -43,13 +70,42 @@ export default function Calendars() {
                     </p>
                   </div>
                   <ChevronRightIcon
-                    className="h-5 w-5 flex-none text-gray-400 ml-16"
+                    className="ml-16 h-5 w-5 flex-none text-gray-400"
                     aria-hidden="true"
                   />
                 </Link>
               </li>
             ))}
           </ul>
+
+          <Button className="w-full" onClick={() => setShowDialog(true)}>
+            Add new calendar
+          </Button>
+          <Dialog isOpen={showDialog} setOpen={setShowDialog}>
+            <Form method="post">
+              <div className="space-y-2 pb-8">
+                <Input name="name" label="Name" />
+                <Input
+                  name="sourceUrl"
+                  label="Source URL"
+                  addonPre="https://"
+                />
+              </div>
+              <div className="flex flex-col gap-2 sm:flex-row sm:gap-4">
+                <Button
+                  size="lg"
+                  color="secondary"
+                  className="w-full"
+                  onClick={() => setShowDialog(false)}
+                >
+                  Cancel
+                </Button>
+                <Button size="lg" className="w-full" type="submit">
+                  Create Calendar
+                </Button>
+              </div>
+            </Form>
+          </Dialog>
         </div>
       </div>
     </div>
