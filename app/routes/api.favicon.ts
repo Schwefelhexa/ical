ef8c03ada91ca8ff89947eb08e38d1a8cdb6b2cb
Alexander Baron <alexander.baron1@rwth-aachen.de>
2023-11-10 10:49:37 +0100

    fix: correct URL generation for favicon detection

--- app/routes/api.favicon.ts
+++ app/routes/api.favicon.ts
@@ -3,7 +3,7 @@ import { redirect, type LoaderFunctionArgs } from "@remix-run/node";
 const lookup = new Map<string, string>();
 
 async function findFaviconUrl(url: URL): Promise<string | null> {
-  const probeUrl = `${url.protocol}${url.hostname}/favicon.ico`;
+  const probeUrl = `${url.protocol}//${url.hostname}/favicon.ico`;
   if (lookup.has(probeUrl)) return lookup.get(probeUrl)!;
 
   console.log("Probing", probeUrl);
@@ -17,7 +17,7 @@ async function findFaviconUrl(url: URL): Promise<string | null> {
   if (hostnameParts.length < 3) return null;
   hostnameParts.shift();
   const newHostname = hostnameParts.join(".");
-  const result = await findFaviconUrl(new URL(`${url.protocol}${newHostname}`));
+  const result = await findFaviconUrl(new URL(`${url.protocol}//${newHostname}`));
   if (!result) return null;
 
   lookup.set(probeUrl, result);
