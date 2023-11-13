1e321b3bbbffc27791ff51d4728fcacd44784420
Alexander Baron <alexander.baron1@rwth-aachen.de>
2023-11-13 18:12:04 +0100

    Respect userId for calendar fetching

--- app/auth.server.ts
+++ app/auth.server.ts
@@ -1,4 +1,5 @@
 import bcrypt from 'bcryptjs';
+import { getSession } from './session.server';
 
 export async function verifyPassword(password: string, hash: string) {
 	return await bcrypt.compare(password, hash);
@@ -6,5 +7,14 @@ export async function verifyPassword(password: string, hash: string) {
 
 export async function hashPassword(password: string) {
 	return await bcrypt.hash(password, 12);
+}
+
+export async function getUserId(request: Request): Promise<string> {
+	const session = await getSession(request.headers.get('Cookie'));
+	if (!session.get("userId")) {
+		throw new Error('Not authenticated');
+	}
+
+	return session.get("userId")!;
 }
 
